9de427011e9df2d05b0d26d3ba5bb4ba71c69a32
pawelkoss <57733708+pawelkoss@users.noreply.github.com>
2021-02-02 13:15:43 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Container, Table, Row, Col, Form  } from 'react-bootstrap';
+import { Container, Table, Row, Col, Form, Modal, Button  } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons';
 import axios from "axios";
 import Add from "./Add";
@@ -8,10 +8,49 @@ import View from "./View";
 import Delete from "./Delete";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function ModalEdit(props) {
+    // {...props} -> show={props.show} onHide={props.onHide} item={props.item}
+    return (
+    <Modal {...props} >
+    <Modal.Header closeButton>
+      <Modal.Title>Modal heading</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <p>tytuł: {props.item.title}</p>
+        <p>autor: {props.item.author}</p>
+        <p>stars: {props.item.rating}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={props.onHide}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={props.onHide}>
+        Save Changes
+      </Button>
+    </Modal.Footer>
+  </Modal> 
+    );
+  }
 
 function App() {
 const [books, setBooks] = useState([]);
 const [loading, setLoading] = useState([false]);
+const [showEdit, setShowEdit] = useState(false);
+const [modalShow, setModalShow] = useState(false);  // ModalEdit
+const [modalData, setModalData] = useState({});
+
+const handleCloseEdit = () => setShowEdit(false);
+const handleShowEdit = () => setShowEdit(true);
+const handleShowEdit2 = (item) => {
+    setModalData(item);
+    setShowEdit(true);
+}
+
+
+const modalShowData = (item) => {
+    setModalData(item);
+    setModalShow(true);
+}
 
 const getBooks = () => {
     setLoading(true);
@@ -42,9 +81,10 @@ const elementView = () => {
         return (
             <tr key={item.id}>
             <View item={item} /> 
-            <td><Icon.PencilSquare color="royalblue" />  <Delete id={item.id} books={books} setBooks={setBooks} getBooks={getBooks} /> <button onClick={() => delBook(item.id)} >X</button></td>
+            <td><Icon.PencilSquare color="royalblue" onClick={() => handleShowEdit2(item)} /> <ModalEdit item={modalData} show={modalShow} onHide={() => setModalShow(false)} />  <Delete id={item.id} books={books} setBooks={setBooks} getBooks={getBooks} /> <button onClick={() => delBook(item.id)} >X</button></td>
             </tr>);
         })
+        //  onClick={() => modalShowData(item)} onClick={handleShowEdit} onClick={() => handleShowEdit2()}
 }
 const elementLoad = () => {
     return(<tr><td>Loading ...</td></tr>)
@@ -73,9 +113,26 @@ return(
         <Col lg="6"><Add books={books} setBooks={setBooks} getBooks={getBooks} /></Col>
     </Row>
     
-    
+  <Modal show={showEdit} onHide={handleCloseEdit} >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!....... {modalData.title}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseEdit}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseEdit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>  
 </Container>
+
+
+      
 );
+
 
 
 /*
